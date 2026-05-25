937325ed1fd89a7bb6df740fa719f1aadf9307d8
Shubham Rai <srai21029@gmail.com>
2026-05-25 06:45:32 +0530

    undo

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/AI_Dungeon_Master",
-  assetPrefix: "/AI_Dungeon_Master/",
+  /* config options here */
 };
 
 export default nextConfig;
+
