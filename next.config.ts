3445404e3b5baf15d7b4448c48dd92d3d22f4e87
Shubham Rai <srai21029@gmail.com>
2026-05-25 06:38:23 +0530

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/AI_Dungeon_Master",
+  assetPrefix: "/AI_Dungeon_Master/",
 };
 
 export default nextConfig;
